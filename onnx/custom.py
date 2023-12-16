df4ab694007b03738eeb934a3367c688be70de80
王盟 <wmnwpu@gmail.com>
2023-12-16 22:36:34 +0800

    onnx

---
+++ onnx/custom.py
@@ -0,0 +1,48 @@
+import onnxscript
+import torch
+from torch.onnx import _type_utils, symbolic_helper
+from torch.onnx._internal import _beartype, jit_utils, registration
+# There are three opset version needed to be aligned
+# This is (1) the opset version in ONNX function
+from onnxscript.onnx_opset import opset15 as op
+opset_version = 15
+
+x = torch.randn(1, 2, 3, 4, requires_grad=True)
+model = torch.nn.SELU()
+
+custom_opset = onnxscript.values.Opset(domain="onnx-script", version=2)
+
+@onnxscript.script(custom_opset)
+def Selu(X):
+    alpha = 1.67326  # auto wrapped as Constants
+    gamma = 1.0507
+    alphaX = op.CastLike(alpha, X)
+    gammaX = op.CastLike(gamma, X)
+    neg = gammaX * (alphaX * op.Exp(X) - alphaX)
+    pos = gammaX * X
+    zero = op.CastLike(0, X)
+    return op.Where(X <= zero, neg, pos)
+
+# setType API provides shape/type to ONNX shape/type inference
+def custom_selu(g: jit_utils.GraphContext, X):
+    return g.onnxscript_op(Selu, X).setType(X.type())
+
+# Register custom symbolic function
+# There are three opset version needed to be aligned
+# This is (2) the opset version in registry
+torch.onnx.register_custom_op_symbolic(
+    symbolic_name="aten::selu",
+    symbolic_fn=custom_selu,
+    opset_version=opset_version,
+)
+
+# There are three opset version needed to be aligned
+# This is (2) the opset version in exporter
+torch.onnx.export(
+    model,
+    x,
+    "model.onnx",
+    opset_version=opset_version,
+    # only needed if you want to specify an opset version > 1.
+    custom_opsets={"onnx-script": 2}
+)
--- onnx/export.py
+++ onnx/export.py
@@ -3,11 +3,9 @@ import torchvision
 import torch._dynamo
 torch._dynamo.config.suppress_errors = True
 
-model = torchvision.models.convnext_tiny(pretrained=True)
+model = torchvision.models.resnet18(pretrained=True)
 model_compile = torch.compile(model)
 
-# torch.onnx.export(model, torch.randn(1, 3, 224, 224), "convnext_tiny.onnx", opset_version=18)
-print(type(model_compile))
-torch.onnx.export(model_compile.eval(), torch.randn(1, 3, 224, 224), "convnext_tiny_compile.onnx")
-# onnx_program = torch.onnx.dynamo_export(model.eval(), torch.randn(1, 3, 224, 224))
-# onnx_program.save("convnext_tiny_dynamo.onnx")
+torch.onnx.export(model, torch.randn(1, 3, 224, 224), "resnet18.onnx", opset_version=18)
+onnx_program = torch.onnx.dynamo_export(model.eval(), torch.randn(1, 3, 224, 224))
+onnx_program.save("resnet18_dynamo.onnx")
